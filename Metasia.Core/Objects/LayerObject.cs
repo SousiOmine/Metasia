5b77e32c3000110ef1890670e9a71a4e9da5f9e5
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2024-08-13 18:28:28 +0900

    レイヤーオブジェクトを作成　中身はない

---
+++ Metasia.Core/Objects/LayerObject.cs
@@ -0,0 +1,29 @@
+﻿using Metasia.Core.Render;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metasia.Core.Objects
+{
+    public class LayerObject : MetasiaObject, IMetaDrawable, IMetaAudiable
+    {
+        public List<MetasiaObject> Objects { get; protected set; } = new();
+        public double Volume { get; set; }
+
+        public LayerObject(string id) : base(id)
+        {
+        }
+
+        public void AudioExpresser(ref AudioExpresserArgs e, int frame)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawExpresser(ref DrawExpresserArgs e, int frame)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
