d94a8478b063c2717e06e5384ca95593a72f0445
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2024-12-31 23:03:55 +0900

    ProjectToolのコンテキストメニューで開くと外部アプリケーションで開くと削除を見た目だけ追加 機能はしない

--- Metasia.Editor/ViewModels/Tools/ProjectToolViewModel.cs
+++ Metasia.Editor/ViewModels/Tools/ProjectToolViewModel.cs
@@ -3,9 +3,11 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices.JavaScript;
+using System.Windows.Input;
 using DynamicData;
 using Metasia.Editor.Models.FileSystem;
 using Metasia.Editor.Models.Tools.ProjectTool;
+using ReactiveUI;
 
 namespace Metasia.Editor.ViewModels.Tools
 {
@@ -14,7 +16,24 @@ namespace Metasia.Editor.ViewModels.Tools
         public string ProjectDir_Path { get; private set; } = String.Empty;
         
         public ObservableCollection<FileTreeNode>? Nodes { get; }
-        
+
+        /// <summary>
+        /// 選択中のノード（複数選択可能）
+        /// </summary>
+        public ObservableCollection<FileTreeNode> SelectedNodes { get; } = new ObservableCollection<FileTreeNode>();
+
+
+
+        public bool IsFileSelecting
+        {
+            get => _isFileSelected;
+            set => this.RaiseAndSetIfChanged(ref _isFileSelected, value);
+        }
+
+        public ICommand OpenFileByExternalApp { get; }
+
+        private bool _isFileSelected;
+
         public ProjectToolViewModel(string? ProjectDir_Path)
         {
             this.ProjectDir_Path = ProjectDir_Path;
@@ -23,6 +42,30 @@ namespace Metasia.Editor.ViewModels.Tools
             {
                 
             }
+
+            OpenFileByExternalApp = ReactiveCommand.Create(() =>
+            {
+                //ファイルやディレクトリを外部アプリで開く処理を書く
+            });
+
+            //選択中のノードが変更された時にコンテキストメニューの表示を変更する
+            SelectedNodes.CollectionChanged += (sender, args) =>
+            {
+                if (SelectedNodes.Count <= 0)
+                {
+                    IsFileSelecting = false;
+                    return;
+                }
+                if (SelectedNodes[0].ResourceEntity is FileEntity)
+                {
+                    IsFileSelecting = true;
+                }
+                else
+                {
+                    IsFileSelecting = false;
+                }
+            };
+
 
             Nodes = new ObservableCollection<FileTreeNode>()
             {
