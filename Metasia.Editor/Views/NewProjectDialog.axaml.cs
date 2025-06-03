b2fdd410e35883ef3b44ed82ccb84326b86a5857
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2025-06-04 03:35:55 +0900

    消えていたViewロジックを再度追加

--- Metasia.Editor/Views/NewProjectDialog.axaml.cs
+++ Metasia.Editor/Views/NewProjectDialog.axaml.cs
@@ -1,7 +1,3 @@
-
-
-
-using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +24,91 @@ namespace Metasia.Editor.Views
         public NewProjectDialog()
         {
             InitializeComponent();
+            LoadTemplates();
+
+            var browseButton = this.FindControl<Button>("BrowseButton");
+            var folderPathTextBox = this.FindControl<TextBox>("FolderPathTextBox");
+            var projectNameTextBox = this.FindControl<TextBox>("ProjectNameTextBox");
+            var cancelButton = this.FindControl<Button>("CancelButton");
+            var createButton = this.FindControl<Button>("CreateButton");
+            var framerateComboBox = this.FindControl<ComboBox>("FramerateComboBox");
+            var resolutionComboBox = this.FindControl<ComboBox>("ResolutionComboBox");
+            var templateComboBox = this.FindControl<ComboBox>("TemplateComboBox");
+
+            templateComboBox.SelectedIndex = 0;
+
+            browseButton.Click += async (sender, e) =>
+            {
+                var folders = await StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+                {
+                    Title = "保存先フォルダを選択",
+                    AllowMultiple = false
+                });
+
+                if (folders.Count > 0)
+                {
+                    folderPathTextBox.Text = folders[0].Path.LocalPath;
+                    UpdateCreateButtonState();
+                }
+            };
+
+            projectNameTextBox.TextChanged += (sender, e) =>
+            {
+                UpdateCreateButtonState();
+            };
+
+            cancelButton.Click += (sender, e) =>
+            {
+                Close(false);
+            };
+
+            createButton.Click += (sender, e) =>
+            {
+                ProjectName = projectNameTextBox.Text;
+                ProjectPath = Path.Combine(folderPathTextBox.Text, ProjectName);
+
+                // フレームレートを取得
+                int framerate = 30;
+                switch (framerateComboBox.SelectedIndex)
+                {
+                    case 0: framerate = 24; break;
+                    case 1: framerate = 30; break;
+                    case 2: framerate = 60; break;
+                }
+
+                // 解像度を取得
+                SKSize size = new SKSize(1920, 1080);
+                switch (resolutionComboBox.SelectedIndex)
+                {
+                    case 0: size = new SKSize(1280, 720); break;
+                    case 1: size = new SKSize(1920, 1080); break;
+                    case 2: size = new SKSize(3840, 2160); break;
+                }
+
+                ProjectInfo = new ProjectInfo
+                {
+                    Framerate = framerate,
+                    Size = size
+                };
+
+                // テンプレートを取得
+                if (templateComboBox.SelectedIndex > 0)
+                {
+                    int templateIndex = templateComboBox.SelectedIndex - 1; // 最初の項目は「空のプロジェクト」
+                    if (templateIndex >= 0 && templateIndex < _availableTemplates.Count)
+                    {
+                        SelectedTemplate = _availableTemplates[templateIndex].Template;
+                    }
+                }
+
+                // プロジェクトフォルダを作成
+                if (!Directory.Exists(ProjectPath))
+                {
+                    Directory.CreateDirectory(ProjectPath);
+                }
+
+                Close(true);
+            };
         }
 
         public void SetTemplates(List<IProjectTemplate> templates)
