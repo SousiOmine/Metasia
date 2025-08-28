7b86c0a4db9dec93ec7c5d3ed64f8b3f1b5d553a
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2025-08-28 16:09:10 +0900

    ClipView.axaml.csのロジックをClipViewBehaviorに移管

--- Metasia.Editor/Views/Behaviors/ClipViewBehavior.cs
+++ Metasia.Editor/Views/Behaviors/ClipViewBehavior.cs
@@ -9,6 +9,8 @@ using Avalonia.Xaml.Interactivity;
 using Metasia.Editor.Models.DragDropData;
 using Metasia.Editor.ViewModels.Controls;
 using Metasia.Editor.Views.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using Metasia.Editor.Services;
 
 namespace Metasia.Editor.Views.Behaviors
 {
@@ -44,9 +46,13 @@ namespace Metasia.Editor.Views.Behaviors
             set => SetValue(FramePerDIPProperty, value);
         }
         
-        private Point? _startPoint;
-        private bool _isDraggingPotential;
-        private string _dragHandleName = string.Empty;
+        // マウス操作の状態管理
+        private DateTime _mousePressStartTime;
+        private bool _hasStartedDrag = false;
+        private const int CLICK_THRESHOLD_MS = 300;
+        
+        private Point? _mousePressStartPoint;
+        private bool _isDragReady = false;
         private bool _isHandleDragging = false;
         
         protected override void OnAttached()
@@ -101,10 +107,24 @@ namespace Metasia.Editor.Views.Behaviors
         
         private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
         {
+            // マウスボタンが押された時間を記録
+            _mousePressStartTime = DateTime.Now;
+            _hasStartedDrag = false;
+            
             if (e.GetCurrentPoint(AssociatedObject).Properties.IsLeftButtonPressed)
             {
-                _startPoint = e.GetCurrentPoint(AssociatedObject).Position;
-                _isDraggingPotential = true;
+                // 左クリックの場合、ドラッグの準備を開始
+                _mousePressStartPoint = e.GetCurrentPoint(AssociatedObject).Position;
+                _isDragReady = true;
+            }
+            else
+            {
+                // 右クリックの場合、即座に選択処理を実行
+                var properties = e.GetCurrentPoint(AssociatedObject).Properties;
+                if (properties.IsRightButtonPressed)
+                {
+                    TryClipSelect(e.KeyModifiers, e);
+                }
             }
         }
         
@@ -112,17 +132,57 @@ namespace Metasia.Editor.Views.Behaviors
         {
             var currentPoint = e.GetCurrentPoint(AssociatedObject).Position;
             
-            if (_isDraggingPotential && e.GetCurrentPoint(AssociatedObject).Properties.IsLeftButtonPressed && _startPoint.HasValue && Math.Abs(currentPoint.X - _startPoint.Value.X) > DragThreshold)
+            // ドラッグの準備ができていて、左ボタンが押されていて、移動距離が閾値を超えている場合
+            if (_isDragReady && e.GetCurrentPoint(AssociatedObject).Properties.IsLeftButtonPressed && _mousePressStartPoint.HasValue && Math.Abs(currentPoint.X - _mousePressStartPoint.Value.X) > DragThreshold)
             {
-                _isDraggingPotential = false;
+                _isDragReady = false;
+                _hasStartedDrag = true; // ドラッグ開始を通知
                 await StartDragDropAsync(e);
             }
         }
         
         private void OnPointerReleased(object? sender, PointerReleasedEventArgs e)
         {
-            _isDraggingPotential = false;
-            _startPoint = null;
+            // マウスボタンが押されていた時間と現在時刻の差を計算
+            var pressDuration = (DateTime.Now - _mousePressStartTime).TotalMilliseconds;
+            
+            // 短時間クリックかつドラッグが開始されていない場合のみ選択処理
+            if (pressDuration < CLICK_THRESHOLD_MS && !_hasStartedDrag)
+            {
+                TryClipSelect(e.KeyModifiers, e);
+            }
+            
+            // 状態をリセット
+            _isDragReady = false;
+            _hasStartedDrag = false;
+            _mousePressStartPoint = null;
+        }
+        
+        private void TryClipSelect(KeyModifiers modifiers, PointerEventArgs? pointerEventArgs)
+        {
+            var vm = AssociatedObject?.DataContext as ClipViewModel;
+            if (vm is null) return;
+
+            // キーバインディングサービスから修飾キー設定を取得
+            var keyBindingService = App.Current?.Services?.GetService<IKeyBindingService>();
+            var multiSelectModifier = keyBindingService?.GetModifierForAction("MultiSelectClip");
+
+            bool isMultiSelect = multiSelectModifier.HasValue && 
+                                keyBindingService != null && keyBindingService.IsModifierKeyPressed(multiSelectModifier.Value, modifiers);
+
+            // クリックされた位置からフレームを計算してViewModelに通知
+            if (pointerEventArgs != null && AssociatedObject != null)
+            {
+                var position = pointerEventArgs.GetCurrentPoint(AssociatedObject).Position;
+                // クリップ内の相対位置を計算
+                var relativePositionX = position.X;
+                // 相対位置をフレームに変換
+                var frameOffset = (int)(relativePositionX / FramePerDIP);
+                // クリップの開始フレームにオフセットを加算して絶対フレーム位置を算出
+                var targetFrame = vm.TargetObject.StartFrame + frameOffset;
+                
+                vm.ClipClick(isMultiSelect, targetFrame);
+            }
         }
 
         /// <summary>
@@ -152,7 +212,6 @@ namespace Metasia.Editor.Views.Behaviors
             e.Handled = true;
             
             _isHandleDragging = true;
-            _dragHandleName = handle.Name;
         }
 
         /// <summary>
@@ -197,7 +256,6 @@ namespace Metasia.Editor.Views.Behaviors
             }
             e.Pointer.Capture(null);
             _isHandleDragging = false;
-            _dragHandleName = string.Empty;
         }
 
         /// <summary>
@@ -214,7 +272,7 @@ namespace Metasia.Editor.Views.Behaviors
         {
             // ViewModelを取得
             var vm = AssociatedObject?.DataContext as ClipViewModel;
-            if (vm is not null && _startPoint.HasValue)
+            if (vm is not null && _mousePressStartPoint.HasValue)
             {
                 // ClipViewにドラッグ開始を通知
                 if (AssociatedObject is ClipView clipView)
@@ -223,7 +281,7 @@ namespace Metasia.Editor.Views.Behaviors
                 }
                 
                 var dragData = new DataObject();
-                dragData.Set(DragDropFormats.ClipsMove, new ClipsMoveDragData(vm, CalculateTargetFrame(_startPoint.Value.X)));
+                dragData.Set(DragDropFormats.ClipsMove, new ClipsMoveDragData(vm, CalculateTargetFrame(_mousePressStartPoint.Value.X)));
                         
                 // 実際のドラッグ&ドロップを開始
                 await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Move);
--- Metasia.Editor/Views/Controls/ClipView.axaml.cs
+++ Metasia.Editor/Views/Controls/ClipView.axaml.cs
@@ -1,102 +1,16 @@
-using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Input;
-using Avalonia.Markup.Xaml;
-using DynamicData.Kernel;
-using Metasia.Editor.Models.DragDropData;
-using Metasia.Editor.ViewModels.Controls;
-using Microsoft.Extensions.DependencyInjection;
-using Metasia.Editor.Services;
 
 namespace Metasia.Editor.Views.Controls;
 
 public partial class ClipView : UserControl
 {
-    private ClipViewModel? VM
-    {
-        get { return this.DataContext as ClipViewModel; }
-    }
-    
-    // 追加するフィールド
-    private DateTime _pointerPressedTime;
-    private bool _isPotentialDrag = false;
-    private const int CLICK_THRESHOLD_MS = 300;
-
     public ClipView()
     {
         InitializeComponent();
-        
-        this.DataContextChanged += (s, e) =>
-        {
-            //ViewModelが置き換えられたときの処理をいつか書く
-        };
-    }
-
-    // PointerPressedイベントハンドラ
-    private void Clip_OnPointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        _pointerPressedTime = DateTime.Now;
-        _isPotentialDrag = false;
-        
-        // 左右どちらのボタンでも処理
-        var properties = e.GetCurrentPoint(this).Properties;
-        if (properties.IsLeftButtonPressed || properties.IsRightButtonPressed)
-        {
-            // 右クリックの場合、即座に選択処理を実行
-            if (properties.IsRightButtonPressed)
-            {
-                // 右クリック時は即座に選択処理を実行
-                TryClipSelect(e.KeyModifiers, e);
-            }
-        }
-    }
-
-    // PointerReleasedイベントハンドラ
-    private void Clip_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
-    {
-        var pressDuration = (DateTime.Now - _pointerPressedTime).TotalMilliseconds;
-        
-        // 短時間クリックかつドラッグが開始されていない場合のみ選択処理
-        if (pressDuration < CLICK_THRESHOLD_MS && !_isPotentialDrag)
-        {
-            TryClipSelect(e.KeyModifiers, e);
-        }
-        
-        _isPotentialDrag = false;
-    }
-
-    private void TryClipSelect(KeyModifiers modifiers, PointerEventArgs? pointerEventArgs)
-    {
-        if (VM is null) return;
-
-        // キーバインディングサービスから修飾キー設定を取得
-        var keyBindingService = App.Current?.Services?.GetService<IKeyBindingService>();
-        var multiSelectModifier = keyBindingService?.GetModifierForAction("MultiSelectClip");
-
-        bool isMultiSelect = multiSelectModifier.HasValue && 
-                            keyBindingService.IsModifierKeyPressed(multiSelectModifier.Value, modifiers);
-
-        // クリックされた位置からフレームを計算してViewModelに通知
-        if (pointerEventArgs != null)
-        {
-            var position = pointerEventArgs.GetCurrentPoint(this).Position;
-            // クリップ内の相対位置を計算
-            var relativePositionX = position.X;
-            // 相対位置をフレームに変換
-            var frameOffset = (int)(relativePositionX / VM.Frame_Per_DIP);
-            // クリップの開始フレームにオフセットを加算して絶対フレーム位置を算出
-            var targetFrame = VM.TargetObject.StartFrame + frameOffset;
-            
-            VM.ClipClick(isMultiSelect, targetFrame);
-        }
     }
     
     // ClipViewBehaviorからのドラッグ開始通知用メソッド
     public void NotifyDragStarted()
     {
-        _isPotentialDrag = true;
     }
 }
