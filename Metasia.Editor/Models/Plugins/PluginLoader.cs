7cdd0b0a52e24dd5e65ed47c81957af66be80919
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2025-10-01 11:09:05 +0900

    プラグインの読み込みパスを、カレントディレクトリ基準ではなくアプリ実行ファイル基準とするように変更

--- Metasia.Editor/Models/Plugins/PluginLoader.cs
+++ Metasia.Editor/Models/Plugins/PluginLoader.cs
@@ -10,15 +10,15 @@ namespace Metasia.Editor.Models.Plugins
 {
     public static class PluginLoader
     {
-        private const string EDITOR_PLUGINS_PATH = "Plugins";
+        private const string EDITOR_PLUGINS_FOLDER_NAME = "Plugins";
 
         private static List<IEditorPlugin> editorPlugins = [];
         
         public static async Task<IEnumerable<IEditorPlugin>> LoadEditorPluginsAsync()
         {
-            if (!Directory.Exists(EDITOR_PLUGINS_PATH))
+            if (!Directory.Exists(Path.Combine(AppContext.BaseDirectory, EDITOR_PLUGINS_FOLDER_NAME)))
             {
-                Directory.CreateDirectory(EDITOR_PLUGINS_PATH);
+                Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, EDITOR_PLUGINS_FOLDER_NAME));
             }
             await LoadPluginsAsync();
             return editorPlugins;
@@ -26,9 +26,9 @@ namespace Metasia.Editor.Models.Plugins
 
         private static async Task LoadPluginsAsync()
         {
-            if (Directory.Exists(EDITOR_PLUGINS_PATH))
+            if (Directory.Exists(Path.Combine(AppContext.BaseDirectory, EDITOR_PLUGINS_FOLDER_NAME)))
             {
-                var pluginFiles = Directory.GetFiles(EDITOR_PLUGINS_PATH, "*.dll", SearchOption.AllDirectories);
+                var pluginFiles = Directory.GetFiles(Path.Combine(AppContext.BaseDirectory, EDITOR_PLUGINS_FOLDER_NAME), "*.dll", SearchOption.AllDirectories);
                 foreach (var pluginFile in pluginFiles)
                 {
                     try
