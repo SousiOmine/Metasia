b1344417b5c503ff98bacb7f22a170afc5fbc53c
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2025-10-22 21:57:04 +0900

    スプラッシュスクリーンみたいな挙動ができる

--- Metasia.Editor/App.axaml.cs
+++ Metasia.Editor/App.axaml.cs
@@ -1,9 +1,12 @@
 
 
 using System;
+using System.Threading.Tasks;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 using Metasia.Editor.Models.EditCommands;
 using Metasia.Editor.Models.Media;
 using Metasia.Editor.Models.States;
@@ -28,6 +31,9 @@ namespace Metasia.Editor
         /// </summary>
         public IServiceProvider? Services { get; private set; }
 
+        private MainWindow? _mainWindow;
+        private Window? _splashScreen;
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -37,63 +43,83 @@ namespace Metasia.Editor
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                // まずMainWindowを作成
-                var mainWindow = new MainWindow();
-                desktop.MainWindow = mainWindow;
+                _splashScreen = new SplashScreen();
+                desktop.MainWindow = _splashScreen;
+                _splashScreen.Show();
 
                 // アプリケーション終了時の処理を登録
                 desktop.ShutdownRequested += OnShutdownRequested;
 
-                // DIコンテナを設定
-                var services = new ServiceCollection();
-                services.AddSingleton<IFileDialogService>(new FileDialogService(mainWindow));
-                services.AddSingleton<INewProjectDialogService, NewProjectDialogService>();
-                services.AddSingleton<IKeyBindingService, KeyBindingService>();
-
-                services.AddSingleton<IEditCommandManager, EditCommandManager>();
-                services.AddSingleton<IAudioService, SoundIOService>();
-                services.AddTransient<IAudioPlaybackService, AudioPlaybackService>();
-
-                services.AddSingleton<IProjectState, ProjectState>();
-                services.AddSingleton<ISelectionState, SelectionState>();
-                services.AddSingleton<IPlaybackState, PlaybackState>();
-                services.AddSingleton<ITimelineViewState, TimelineViewState>();
-
-                services.AddSingleton<MediaAccessorRouter>();
-                services.AddSingleton<IPluginService, PluginService>();
-
-                services.AddTransient<IPlayerViewModelFactory, PlayerViewModelFactory>();
-                services.AddTransient<ITimelineViewModelFactory, TimelineViewModelFactory>();
-                services.AddTransient<ILayerButtonViewModelFactory, LayerButtonViewModelFactory>();
-                services.AddTransient<ILayerCanvasViewModelFactory, LayerCanvasViewModelFactory>();
-                services.AddTransient<IClipViewModelFactory, ClipViewModelFactory>();
-                services.AddTransient<IPropertyRouterViewModelFactory, PropertyRouterViewModelFactory>();
-                services.AddTransient<IClipSettingPaneViewModelFactory, ClipSettingPaneViewModelFactory>();
-                services.AddTransient<IMetaNumberParamPropertyViewModelFactory, MetaNumberParamPropertyViewModelFactory>();
-                services.AddTransient<IMediaPathPropertyViewModelFactory, MediaPathPropertyViewModelFactory>();
-                services.AddTransient<IStringPropertyViewModelFactory, StringPropertyViewModelFactory>();
-                services.AddTransient<IDoublePropertyViewModelFactory, DoublePropertyViewModelFactory>();
-
-
-                services.AddTransient<MainWindowViewModel>();
-                services.AddSingleton<PlayerParentViewModel>();
-                services.AddSingleton<TimelineParentViewModel>();
-                services.AddSingleton<InspectorViewModel>();
-                services.AddSingleton<ToolsViewModel>();
-                Services = services.BuildServiceProvider(new ServiceProviderOptions
+                LoadBeforeApplicationStartAsync().ContinueWith(_ =>
                 {
-                    ValidateScopes = true,
-                    ValidateOnBuild = true
+                    Dispatcher.UIThread.Post(() => CompleteApplicationStart());
                 });
 
-                // プラグインを読み込み
-                Services.GetRequiredService<IPluginService>().LoadPluginsAsync();
-
-                // DIコンテナが設定された後にViewModelを作成
-                mainWindow.DataContext = Services.GetRequiredService<MainWindowViewModel>();
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private async Task LoadBeforeApplicationStartAsync()
+        {
+            _mainWindow = new MainWindow();
+            var services = new ServiceCollection();
+            services.AddSingleton<IFileDialogService>(new FileDialogService(_mainWindow));
+            services.AddSingleton<INewProjectDialogService, NewProjectDialogService>();
+            services.AddSingleton<IKeyBindingService, KeyBindingService>();
+
+            services.AddSingleton<IEditCommandManager, EditCommandManager>();
+            services.AddSingleton<IAudioService, SoundIOService>();
+            services.AddTransient<IAudioPlaybackService, AudioPlaybackService>();
+
+            services.AddSingleton<IProjectState, ProjectState>();
+            services.AddSingleton<ISelectionState, SelectionState>();
+            services.AddSingleton<IPlaybackState, PlaybackState>();
+            services.AddSingleton<ITimelineViewState, TimelineViewState>();
+
+            services.AddSingleton<MediaAccessorRouter>();
+            services.AddSingleton<IPluginService, PluginService>();
+
+            services.AddTransient<IPlayerViewModelFactory, PlayerViewModelFactory>();
+            services.AddTransient<ITimelineViewModelFactory, TimelineViewModelFactory>();
+            services.AddTransient<ILayerButtonViewModelFactory, LayerButtonViewModelFactory>();
+            services.AddTransient<ILayerCanvasViewModelFactory, LayerCanvasViewModelFactory>();
+            services.AddTransient<IClipViewModelFactory, ClipViewModelFactory>();
+            services.AddTransient<IPropertyRouterViewModelFactory, PropertyRouterViewModelFactory>();
+            services.AddTransient<IClipSettingPaneViewModelFactory, ClipSettingPaneViewModelFactory>();
+            services.AddTransient<IMetaNumberParamPropertyViewModelFactory, MetaNumberParamPropertyViewModelFactory>();
+            services.AddTransient<IMediaPathPropertyViewModelFactory, MediaPathPropertyViewModelFactory>();
+            services.AddTransient<IStringPropertyViewModelFactory, StringPropertyViewModelFactory>();
+            services.AddTransient<IDoublePropertyViewModelFactory, DoublePropertyViewModelFactory>();
+
+
+            services.AddTransient<MainWindowViewModel>();
+            services.AddSingleton<PlayerParentViewModel>();
+            services.AddSingleton<TimelineParentViewModel>();
+            services.AddSingleton<InspectorViewModel>();
+            services.AddSingleton<ToolsViewModel>();
+            Services = services.BuildServiceProvider(new ServiceProviderOptions
+            {
+                ValidateScopes = true,
+                ValidateOnBuild = true
+            });
+
+            // プラグインを読み込み
+            await Services.GetRequiredService<IPluginService>().LoadPluginsAsync();
+
+        }
+
+        private void CompleteApplicationStart()
+        {
+            if (ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop) return;
+
+
+            _mainWindow!.DataContext = Services.GetRequiredService<MainWindowViewModel>();
+            desktop.MainWindow = _mainWindow;
+
+            _mainWindow!.Show();
+
+            _splashScreen!.Close();
         }
 
         private void OnShutdownRequested(object? sender, ShutdownRequestedEventArgs e)
