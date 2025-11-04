10fb22b9d77aaab5468c3f87884358043e0d7bc7
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2025-11-05 06:46:23 +0900

    標準入力の簡易画像キャッシュ

--- Metasia.Editor/Models/Media/StandardInput/StdInput.cs
+++ Metasia.Editor/Models/Media/StandardInput/StdInput.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.IO;
 using System.Threading.Tasks;
 using Metasia.Core.Media;
@@ -7,6 +8,8 @@ namespace Metasia.Editor.Models.Media.StandardInput;
 
 public class StdInput : IImageFileAccessor
 {
+    private static readonly ConcurrentDictionary<string, SKBitmap> _imageCache = new();
+    
     public async Task<ImageFileAccessorResult> GetBitmapAsync(MediaPath path)
     {
         var fullPath = MediaPath.GetFullPath(path, "");
@@ -15,7 +18,17 @@ public class StdInput : IImageFileAccessor
             return new ImageFileAccessorResult { IsSuccessful = false, Bitmap = null };
         }
 
+        if (_imageCache.TryGetValue(fullPath, out var cachedBitmap))
+        {
+            return new ImageFileAccessorResult { IsSuccessful = true, Bitmap = cachedBitmap };
+        }
+
         SKBitmap bitmap = SKBitmap.Decode(fullPath);
+        if (bitmap != null)
+        {
+            _imageCache.TryAdd(fullPath, bitmap);
+        }
+        
         return new ImageFileAccessorResult { IsSuccessful = true, Bitmap = bitmap };
     }
 }
