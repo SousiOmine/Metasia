f3a53535883e8b5a1fc97bbe46ca05655d28120e
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2026-01-14 18:56:17 +0900

    とりあえずIEncoder作る

---
+++ Metasia.Core/Encode/IEncoder.cs
@@ -0,0 +1,43 @@
+namespace Metasia.Core.Encode;
+
+/// <summary>
+/// プロジェクトをエンコードするオブジェクトの抽象化インターフェース
+/// </summary>
+public interface IEncoder
+{
+    /// <summary>
+    /// エンコード過程の進捗率を0~100で表す
+    /// </summary>
+    double ProgressRate { get; }
+    
+    /// <summary>
+    /// エンコードの進捗に変化があった時に呼び出される
+    /// </summary>
+    event EventHandler<EventArgs> StatusChanged;
+
+    /// <summary>
+    /// エンコードが開始された時に呼び出される
+    /// </summary>
+    event EventHandler<EventArgs> EncodeStarted;
+
+    /// <summary>
+    /// エンコードが正常に終了した時に呼び出される
+    /// </summary>
+    event EventHandler<EventArgs> EncodeCompleted;
+
+    /// <summary>
+    /// エンコードが異常終了した時に呼び出される
+    /// </summary>
+    event EventHandler<EventArgs> EncodeFailed;
+
+    /// <summary>
+    /// エンコードを開始する
+    /// </summary>
+    void Start();
+    
+    /// <summary>
+    /// エンコードを中止する
+    /// </summary>
+    void CancelRequest();
+}
+}
