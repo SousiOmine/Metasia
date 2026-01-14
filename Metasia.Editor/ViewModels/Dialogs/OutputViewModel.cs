d7979c85efed933caff314a1c5ec7916fa947b1c
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2026-01-14 16:32:05 +0900

    適当にボタンを追加 キャンセルを押したら閉じるようになった

--- Metasia.Editor/ViewModels/Dialogs/OutputViewModel.cs
+++ Metasia.Editor/ViewModels/Dialogs/OutputViewModel.cs
@@ -1,7 +1,37 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using ReactiveUI;
+
 namespace Metasia.Editor.ViewModels.Dialogs;
 
 public class OutputViewModel : ViewModelBase
 {
+
+    public ObservableCollection<string> OutputMethodList { get; } = new ObservableCollection<string>();
+
+    public ObservableCollection<string> TimelineList { get; } = new ObservableCollection<string>();
+
+    public ICommand CancelCommand { get; }
+
+    public Action? CancelAction { get; set; }
+
+    public OutputViewModel()
+    {
+        CancelCommand = ReactiveCommand.Create(() => Cancel());
+
+        OutputMethodList.Add("連番画像出力");
+        OutputMethodList.Add("FFmpegPlugin");
+
+        TimelineList.Add("RootTimeline");
+        TimelineList.Add("Timeline1");
+        TimelineList.Add("Timeline2");
+    }
+
+    private void Cancel()
+    {
+        CancelAction?.Invoke();
+    }
 
 }
 
--- Metasia.Editor/Views/Dialogs/OutputWindow.axaml.cs
+++ Metasia.Editor/Views/Dialogs/OutputWindow.axaml.cs
@@ -1,13 +1,28 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Metasia.Editor.ViewModels.Dialogs;
 
 namespace Metasia.Editor.Views.Dialogs;
 
 public partial class OutputWindow : Window
 {
+    private OutputViewModel? _viewModel;
     public OutputWindow()
     {
         InitializeComponent();
+
+        this.DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object? sender, EventArgs args)
+    {
+        _viewModel = this.DataContext as OutputViewModel;
+
+        if (_viewModel is not null)
+        {
+            _viewModel.CancelAction = () => this.Close();
+        }
     }
 }
