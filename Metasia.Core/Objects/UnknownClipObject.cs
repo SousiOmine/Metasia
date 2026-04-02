c2b37e52b16fd5a2742790ed7234c3b0c4d8752d
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2026-04-03 08:04:10 +0900

    いくらか修正

--- Metasia.Core/Objects/UnknownClipObject.cs
+++ Metasia.Core/Objects/UnknownClipObject.cs
@@ -5,23 +5,58 @@ namespace Metasia.Core.Objects;
 
 public class UnknownClipObject : ClipObject, IUnknownMetasiaObject
 {
+    private const string UnknownXsiTypeAttributeName = "unknownXsiType";
+    private const string XsiNamespace = "http://www.w3.org/2001/XMLSchema-instance";
+
+    [XmlAnyAttribute]
+    public XmlAttribute[] RawAttributes { get; set; } = Array.Empty<XmlAttribute>();
+
     [XmlAnyElement]
     public XmlElement[] RawElements { get; set; } = Array.Empty<XmlElement>();
 
     public string RawXml
     {
-        get => RawElements.Length > 0 ? RawElements[0].OuterXml : string.Empty;
-        set
+        get
         {
-            if (string.IsNullOrWhiteSpace(value))
+            var document = new XmlDocument();
+            var root = document.CreateElement(nameof(ClipObject));
+
+            foreach (var attribute in RawAttributes ?? Array.Empty<XmlAttribute>())
             {
-                RawElements = Array.Empty<XmlElement>();
-                return;
+                AppendAttribute(document, root, attribute);
             }
 
-            var document = new XmlDocument();
-            document.LoadXml(value);
-            RawElements = [document.DocumentElement!];
+            AppendElement(document, root, nameof(StartFrame), StartFrame.ToString());
+            AppendElement(document, root, nameof(EndFrame), EndFrame.ToString());
+            AppendElement(document, root, nameof(Id), Id);
+            AppendElement(document, root, nameof(IsActive), IsActive.ToString().ToLowerInvariant());
+
+            foreach (var element in RawElements ?? Array.Empty<XmlElement>())
+            {
+                root.AppendChild(document.ImportNode(element, true));
+            }
+
+            return root.OuterXml;
         }
+    }
+
+    private static void AppendElement(XmlDocument document, XmlElement parent, string name, string value)
+    {
+        var element = document.CreateElement(name);
+        element.InnerText = value;
+        parent.AppendChild(element);
+    }
+
+    private static void AppendAttribute(XmlDocument document, XmlElement parent, XmlAttribute attribute)
+    {
+        if (attribute.LocalName == UnknownXsiTypeAttributeName && string.IsNullOrEmpty(attribute.NamespaceURI))
+        {
+            var xsiTypeAttribute = document.CreateAttribute("xsi", "type", XsiNamespace);
+            xsiTypeAttribute.Value = attribute.Value;
+            parent.Attributes.Append(xsiTypeAttribute);
+            return;
+        }
+
+        parent.Attributes.Append((XmlAttribute)document.ImportNode(attribute, true));
     }
 }
